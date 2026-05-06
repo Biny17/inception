0bb89995286c98f2b6821cd15977d0f8fb7a0946
Biny17 <tristangallet17@gmail.com>
2026-05-07 01:07:51 +0200

    fixing the wordpress

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 all: up
 
-up: secrets secrets/db_password.txt secrets/db_root_password.txt secrets/cert.key secrets/cert.pem
+up: secrets \
+		secrets/db_password.txt \
+		secrets/db_root_password.txt \
+		secrets/cert.key \
+		secrets/cert.pem \
+		secrets/wp_password.txt \
+		secrets/wp_admin.txt
 	docker compose -f srcs/compose.yaml up
 
 down:
@@ -21,18 +27,21 @@ secrets:
 	mkdir -p secrets
 
 secrets/db_password.txt: secrets
-	openssl rand -base64 8 > secrets/db_password.txt
+	openssl rand -base64 10 > $@
 
 secrets/db_root_password.txt: secrets
-	openssl rand -base64 8 > secrets/db_root_password.txt
+	openssl rand -base64 10 > $@
 
 secrets/cert.key: secrets
-	openssl genrsa -out secrets/cert.key 4096
+	openssl genrsa -out $@ 4096
 
 secrets/cert.pem: secrets secrets/cert.key
-	openssl req -x509 -key secrets/cert.key -out secrets/cert.pem \
+	openssl req -x509 -key secrets/cert.key -out $@ \
 		-sha256 -days 365 -nodes \
 		-subj "/CN=localhost"
 
-.PHONY: up
+secrets/wp_password.txt: secrets
+	openssl rand -base64 10 > $@
+
+.PHONY: up down stop re clean fclean
 
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
-set -e
 
-DB_PASSWORD=$(cat ${WORDPRESS_DB_PASSWORD_FILE})
-WP_URL="https://${DOMAIN_NAME}"
+SQL_USER=$WORDPRESS_DB_USER
+SQL_PASSWORD=$(cat $WORDPRESS_DB_PASSWORD_FILE)
+WP_ADMIN_USER=$(cat $WORDPRESS_ADMIN_USER_FILE)
+WP_ADMIN_PASSWORD=$(cat $WORDPRESS_ADMIN_PASSWORD_FILE)
 
-echo "Waiting for MariaDB..."
-while ! nc -z db 3306; do
+SQL_DATABASE=$WORDPRESS_DB_NAME
+
+while ! nc -z $WORDPRESS_DB_HOST 3306; do
     sleep 1
 done
 
-if [ ! -f /var/www/html/wp-settings.php ]; then
-    mkdir -p /var/www/html
-    cp -a /usr/src/wordpress/. /var/www/html/
+if [ ! -f ./wp-config.php ]; then
+    cp -a /usr/src/wordpress/. .
+
+    wp config create \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost=$WORDPRESS_DB_HOST \
+        --allow-root
+
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="$WP_TITLE" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
+
+    wp user create editor editor@$DOMAIN_NAME --role=editor --user_pass=editor42pass --allow-root
+
 fi
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+chmod -R 755 /var/www/html
 
-    sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" /var/www/html/wp-config.php
-    sed -i "s/username_here/${WORDPRESS_DB_USER}/" /var/www/html/wp-config.php
-    sed -i "s/password_here/${DB_PASSWORD}/" /var/www/html/wp-config.php
-    sed -i "s/localhost/db/" /var/www/html/wp-config.php
-fi
-
-sed -i "/define( *'WP_HOME'/d" /var/www/html/wp-config.php
-sed -i "/define( *'WP_SITEURL'/d" /var/www/html/wp-config.php
-sed -i "/define( *'WP_REDIS_HOST'/d" /var/www/html/wp-config.php
-sed -i "/define( *'WP_REDIS_PORT'/d" /var/www/html/wp-config.php
-sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i define('WP_HOME', '${WP_URL}');" /var/www/html/wp-config.php
-sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i define('WP_SITEURL', '${WP_URL}');" /var/www/html/wp-config.php
-sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i define('WP_REDIS_HOST', 'bonus');" /var/www/html/wp-config.php
-sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i define('WP_REDIS_PORT', 6379);" /var/www/html/wp-config.php
-
-php-fpm8.2 -F
+exec php-fpm8.2 -F
